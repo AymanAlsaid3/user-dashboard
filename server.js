ef4afe515d0958bc4506564180c946dc63c2620b
AymanAlsaid3 <Aymanala39eed@gmail.com>
2026-09-17 19:28:30 +0300

    Apply deployment fixes

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ import express from 'express';
 import userRoutes from './routes/users.js';
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology:True})
-    .then(() => console.log('Connected to MongoDB successfully!'))
-    .catch(err => console.error('Failed to connect to MongoDB:', err));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
